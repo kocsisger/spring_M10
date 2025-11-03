0d678c4c924f598b2d923ce40368a42f5f0b97fa
Kocsis Gergely <kocsisger@example.com>
2025-11-03 10:46:46 +0100

    Repository added

--- src/main/java/en/unideb/inf/demo/model/Person.java
+++ src/main/java/en/unideb/inf/demo/model/Person.java
@@ -1,11 +1,23 @@
 package en.unideb.inf.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue
     long id;
     String name;
     LocalDate dateOfBirth;
