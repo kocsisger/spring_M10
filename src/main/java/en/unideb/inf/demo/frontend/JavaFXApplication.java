2038d4b200057a210f55c79d532392bec30723a7
Kocsis Gergely <kocsisger@example.com>
2025-11-10 10:20:59 +0100

    JavaFX and Spring connected

--- src/main/java/en/unideb/inf/demo/frontend/JavaFXApplication.java
+++ src/main/java/en/unideb/inf/demo/frontend/JavaFXApplication.java
@@ -1,17 +1,29 @@
 package en.unideb.inf.demo.frontend;
 
+import en.unideb.inf.demo.SpringDataFxApplication;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 public class JavaFXApplication extends Application {
+    private ConfigurableApplicationContext ctx;
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(
                 getClass().getResource("/fxml/MainPage.fxml"));
         stage.setScene(new Scene(root));
         stage.show();
+        ctx = SpringApplication.run(SpringDataFxApplication.class);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        ctx.close();
+        super.stop();
     }
 }
